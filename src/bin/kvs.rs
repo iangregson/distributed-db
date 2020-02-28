02274f35f05b943dcba5a85d935aaab484e13856
ian <ian@gregson.me>
2020-02-28 13:11:01 +0000

    clippy/fmt

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,5 +1,5 @@
 extern crate clap;
-use clap::{App, Arg, AppSettings, SubCommand};
+use clap::{App, AppSettings, Arg, SubCommand};
 
 fn main() {
     let matches = App::new(env!("CARGO_PKG_NAME"))
@@ -9,22 +9,29 @@ fn main() {
         .subcommand(
             SubCommand::with_name("get")
                 .about("Get the string value of a given string key")
-                .arg(Arg::with_name("KEY").help("A string key").required(true)))
+                .arg(Arg::with_name("KEY").help("A string key").required(true)),
+        )
         .subcommand(
             SubCommand::with_name("set")
                 .about("Set the string value of a given string key")
                 .arg(Arg::with_name("KEY").help("A string key").required(true))
-                .arg(Arg::with_name("VALUE").help("A string value").required(true)))
+                .arg(
+                    Arg::with_name("VALUE")
+                        .help("A string value")
+                        .required(true),
+                ),
+        )
         .subcommand(
             SubCommand::with_name("rm")
                 .about("Remove the given key")
-                .arg(Arg::with_name("KEY").help("A string key").required(true)))
+                .arg(Arg::with_name("KEY").help("A string key").required(true)),
+        )
         .get_matches();
 
     match matches.subcommand() {
         ("get", Some(_matches)) => panic!("unimplemented"),
         ("set", Some(_matches)) => panic!("unimplemented"),
         ("rm", Some(_matches)) => panic!("unimplemented"),
-        _ => unreachable!()
+        _ => unreachable!(),
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,30 @@ use std::collections::HashMap;
 
 #[derive(Default)]
 pub struct KvStore {
-  map: HashMap<String, String>
+    map: HashMap<String, String>,
 }
 
 impl KvStore {
-  /// Creates a new `KvStore`
-  pub fn new() -> KvStore {
-    KvStore {
-      map: HashMap::new(),
+    /// Creates a new `KvStore`
+    pub fn new() -> KvStore {
+        KvStore {
+            map: HashMap::new(),
+        }
     }
-  }
-  /// Sets the value of a string key to string
-  ///
-  /// If the key exists, the previous value is overwritten
-  pub fn set(&mut self, key: String, value: String) {
-    self.map.insert(key, value);
-  }
-  /// Gets the string value of the given string key
-  ///
-  /// If the key does not exist, None is returned
-  pub fn get(&self, key: String) -> Option<String> {
-    self.map.get(&key).cloned()
-  }
-  /// Removes a given key from the store
-  pub fn remove(&mut self, key: String) {
-    self.map.remove(&key);
-  }
+    /// Sets the value of a string key to string
+    ///
+    /// If the key exists, the previous value is overwritten
+    pub fn set(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
+    }
+    /// Gets the string value of the given string key
+    ///
+    /// If the key does not exist, None is returned
+    pub fn get(&self, key: String) -> Option<String> {
+        self.map.get(&key).cloned()
+    }
+    /// Removes a given key from the store
+    pub fn remove(&mut self, key: String) {
+        self.map.remove(&key);
+    }
 }
-
