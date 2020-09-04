9cc03a344346f7ca07cadbf9d58316aa9c2fa816
ian <ian@gregson.me>
2020-09-04 13:09:59 +0000

    Project 4 tests pass

--- src/engines/mod.rs
+++ src/engines/mod.rs
@@ -7,19 +7,19 @@ pub trait KvsEngine {
     /// Sets the value of a string key to string.
     ///
     /// If the key exists, the prvious value is overwritten
-    fn set(&mut self, key: String, value: String) -> Result<()>;
+    fn set(&self, key: String, value: String) -> Result<()>;
 
     /// Gets the string value of a string key.
     ///
     /// Returns `None` if the given key is not found.
-    fn get(&mut self, key: String) -> Result<Option<String>>;
+    fn get(&self, key: String) -> Result<Option<String>>;
 
     /// Removes a given key.
     ///
     /// # Errors
     ///
     /// It returns `KvsError::KeyNotFound` if the given key is not found.
-    fn remove(&mut self, key: String) -> Result<()>;
+    fn remove(&self, key: String) -> Result<()>;
 }
 
 mod kvs;
